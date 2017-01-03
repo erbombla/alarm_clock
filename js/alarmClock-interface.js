432736885cf6890c7fe723dfb3c9b234c15a78ea
Eric Bomblatus and Matt Eichsteadt <matt@>
2017-01-03 11:04:17 -0800

    add countdown for seconds

--- js/alarmClock-interface.js
+++ js/alarmClock-interface.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-  $('#time').text(moment());
+$( function(){
+  function refresh() {
+    hour = moment().format('h')
+    minute = moment().format('mm')
+    second = moment().format('ss')
+    suffix = moment().format('a')
+    time = moment().format('h:mm:ss a')
+    $('#time').text(time);
+  }
+  setInterval(refresh, 1000);
 });
